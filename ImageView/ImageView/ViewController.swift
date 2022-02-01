e04e1968ae04c85c153913432b369c4f5ec0e677
ehvkddl <ehvkddl@naver.com>
2022-02-02 03:28:55 +0900

    Practice ImageView

---
+++ ImageView/ImageView/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by do hee kim on 2022/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+    
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0 //이미지 확대할 배율값
+        var newWidth: CGFloat, newHeight: CGFloat //이미지 확대/축소했을 때, 이미지의 가로, 세로 길이
+        
+        if (isZoom) { //현재 상태가 확대일 때
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            btnResize.setTitle("확대", for: .normal)
+        } else { //현재 상태가 축소일 때
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+}
+
