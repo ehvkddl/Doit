8542b299fc435a119b29a293f4c4e683650fe90f
ehvkddl <ehvkddl@naver.com>
2022-02-18 18:19:22 +0900

    알람 시계 만들기

--- DatePickerMission/DatePickerMission/ViewController.swift
+++ DatePickerMission/DatePickerMission/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     let timeSelector: Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     var count = 0
+    var alarmTime: String?
     
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
@@ -29,7 +30,7 @@ class ViewController: UIViewController {
         lblPickerTime.text = "선택시간 : " + formatter.string(from: datePickerView.date)
         
         formatter.dateFormat = "hh:mm aaa"
-        )
+        alarmTime = formatter.string(from: datePickerView.date)
     }
     
     @objc func updateTime() {
@@ -37,6 +38,15 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblCurrentTime.text = "현재시간 : " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if (alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
 }
 
